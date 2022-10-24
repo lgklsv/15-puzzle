5f5f252219b0e4151d8ae42173d317e3dfdf4cd5
Oleg Kolosov <kolosov20011@gmail.com>
2022-10-24 23:00:57 +0300

    feat: add drag and drop with mouse for tiles

--- codejam-the-gem-puzzle/src/controller.js
+++ codejam-the-gem-puzzle/src/controller.js
@@ -21,6 +21,7 @@ let winArr = [];
 let curTile = null;
 let moves = 0, time = 0;
 let gameTimer;
+let dragAndDropDone = false;
 
 let start = 0;
 
@@ -28,7 +29,7 @@ canvas.addEventListener('click', function(e) {
     const clientX = e.offsetX;
     const clientY = e.offsetY;
 
-    if(gameOver) {
+    if(gameOver && !dragAndDropDone) {
         gameOver = false;
         moves = 0;
         time = 0;
@@ -53,17 +54,9 @@ canvas.addEventListener('click', function(e) {
     let rowIndex;
     
     const movesEl = document.querySelector('.moves-number');
-    if(moves === 0) {
-        const secondsEl = document.querySelector('.seconds');
-        clearInterval(gameTimer);
-        gameTimer = setInterval(() => {
-            time++;
-            secondsEl.textContent = formatSeconds(time);
-        }, 1000);
-    }
+    startGameTimer(moves);
     
     for(let i = 0; i < state.gameSize; i++) {
-
         let curTileID = coords[i].find(el => {
             return clientX > el.x && clientX < el.x + tileSize && clientY > el.y && clientY < el.y + tileSize;
         })
@@ -71,28 +64,13 @@ canvas.addEventListener('click', function(e) {
         if(curTileID) {
             colIndex = i;
             rowIndex = coords[i].indexOf(curTileID);
-
             curTile = field[colIndex][rowIndex];
         }
     }
+    const emptyTile = getEmptyTile(colIndex, rowIndex);
 
-    const getEmptyTile = function() {
-        if(field[colIndex]?.[rowIndex - 1] == 0) {
-            return {col: colIndex, row: rowIndex - 1};
-        }
-        if(field[colIndex]?.[rowIndex + 1] == 0) {
-            return {col: colIndex, row: rowIndex + 1};
-        }
-        if(field[colIndex - 1 ]?.[rowIndex] == 0) {
-            return {col: colIndex - 1, row: rowIndex};
-        }
-        if(field[colIndex + 1]?.[rowIndex] == 0) {
-            return {col: colIndex + 1, row: rowIndex};
-        }
-    }
-    const emptyTile = getEmptyTile();
+    if(emptyTile && !dragAndDropDone) {
 
-    if(emptyTile) {
         const temp = field[colIndex][rowIndex];
         field[colIndex][rowIndex] = 0;
         field[emptyTile.col][emptyTile.row] = temp;
@@ -105,7 +83,34 @@ canvas.addEventListener('click', function(e) {
         }
         gameOver = checkWin();
     }
+    dragAndDropDone = false;
 })
+
+function startGameTimer(moves) {
+    if(moves === 0) {
+        const secondsEl = document.querySelector('.seconds');
+        clearInterval(gameTimer);
+        gameTimer = setInterval(() => {
+            time++;
+            secondsEl.textContent = formatSeconds(time);
+        }, 1000);
+    }
+}
+
+function getEmptyTile(colIndex, rowIndex) {
+    if(field[colIndex]?.[rowIndex - 1] == 0) {
+        return {col: colIndex, row: rowIndex - 1};
+    }
+    if(field[colIndex]?.[rowIndex + 1] == 0) {
+        return {col: colIndex, row: rowIndex + 1};
+    }
+    if(field[colIndex - 1 ]?.[rowIndex] == 0) {
+        return {col: colIndex - 1, row: rowIndex};
+    }
+    if(field[colIndex + 1]?.[rowIndex] == 0) {
+        return {col: colIndex + 1, row: rowIndex};
+    }
+}
 
 function swap(directionX, directionY, rowIndex, colIndex) {
     const temp = field[rowIndex + directionX][colIndex + directionY];
@@ -116,7 +121,6 @@ function swap(directionX, directionY, rowIndex, colIndex) {
 function suffleField(times) {
     let indexRow, indexCol;
     for(let i = 0; i < times; i++) {
-        let swapEl;
         for(let j = 0; j < state.gameSize; j++) {
             if(field[j].indexOf(0) !== -1) {
                 indexRow = j;
@@ -125,28 +129,24 @@ function suffleField(times) {
                 if(randomMoveDir == 1) {
                     //left
                     if(field[indexRow]?.[indexCol - 1]) {
-                        swapEl = field[indexRow][indexCol - 1];
                         swap(0, -1, indexRow, indexCol);
                     }
                 }
                 if(randomMoveDir == 2) {
                     // top
                     if(field[indexRow - 1]?.[indexCol]) {
-                        swapEl = field[indexRow - 1][indexCol];
                         swap(-1, 0, indexRow, indexCol);
                     }
                 }
                 if(randomMoveDir == 3) {
                     // right
                     if(field[indexRow]?.[indexCol + 1]) {
-                        swapEl = field[indexRow][indexCol + 1];
                         swap(0, 1, indexRow, indexCol);
                     }
                 }
                 if(randomMoveDir == 4) {
                     // bottom
                     if(field[indexRow + 1]?.[indexCol]) {
-                        swapEl = field[indexRow + 1][indexCol];
                         swap(1, 0, indexRow, indexCol);
                     }
                 }
@@ -191,28 +191,27 @@ function checkWin() {
 }
 
 function showGameWinScreen(winTime) {
-
     if(canvas.width == 350) {
         context.font = '26px Roboto';
         context.fillStyle = '#3c3c48';
         context.textAlign = 'center';
-        context.fillText(`Hooray! 🎉🎉🎉`, 175, 80);
-        context.fillText(`You solved the puzzle in`, 175, 120);
-        context.fillText(`${formatSeconds(winTime)}`, 175, 160);
-        context.fillText(`and ${moves} moves!`, 175, 200);
-        context.fillText(`Click to continue 🔁`, 175, 290);
+        context.fillText(`Hooray! 🎉🎉🎉`, 175, 60);
+        context.fillText(`You solved the puzzle in`, 175, 100);
+        context.fillText(`${formatSeconds(winTime)}`, 175, 140);
+        context.fillText(`and ${moves} moves!`, 175, 180);
+        context.fillText(`Click to continue 🔁`, 175, 250);
     }
     if (canvas.width == 300) {
         context.font = '24px Roboto';
         context.fillStyle = '#3c3c48';
         context.textAlign = 'center';
-        context.fillText(`Hooray! 🎉🎉🎉`, 150, 80);
-        context.fillText(`You solved the puzzle in`, 150, 120);
-        context.fillText(`${formatSeconds(winTime)}`, 150, 160);
-        context.fillText(`and ${moves} moves!`, 150, 200);
-        context.fillText(`Click to continue 🔁`, 150, 250);
+        context.fillText(`Hooray! 🎉🎉🎉`, 150, 40);
+        context.fillText(`You solved the puzzle in`, 150, 80);
+        context.fillText(`${formatSeconds(winTime)}`, 150, 120);
+        context.fillText(`and ${moves} moves!`, 150, 160);
+        context.fillText(`Click to continue 🔁`, 150, 230);
     }
-    else {
+    if (canvas.width == 400) {
         context.font = '32px Roboto';
         context.fillStyle = '#3c3c48';
         context.textAlign = 'center';
@@ -577,3 +576,58 @@ tileSoundSwitcher.addEventListener('change', function(e) {
         persistState();
     }
 })
+
+function swipeDetect(el) {
+    let surface = el;
+    let colIndex;
+    let rowIndex;
+    const movesEl = document.querySelector('.moves-number');
+    surface.addEventListener('mousedown', function(e) {
+        e.preventDefault();
+        const rect = canvas.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        for(let i = 0; i < state.gameSize; i++) {
+            let curTileID = coords[i].find(el => {
+                return x > el.x && x < el.x + tileSize && y > el.y && y < el.y + tileSize;
+            })
+            if(curTileID) {
+                colIndex = i;
+                rowIndex = coords[i].indexOf(curTileID);
+                curTile = field[colIndex][rowIndex];
+            }
+        }
+    })
+
+    surface.addEventListener('mouseup', function(e) {
+        e.preventDefault();
+        const rect = canvas.getBoundingClientRect();
+        const x = e.clientX - rect.left;
+        const y = e.clientY - rect.top;
+
+        startGameTimer(moves);
+        const emptyTile = getEmptyTile(colIndex, rowIndex);
+        
+        if(emptyTile) {
+            const emptyTileCoords = coords[emptyTile.col][emptyTile.row];
+            if(x > emptyTileCoords.x && x < emptyTileCoords.x + tileSize && y > emptyTileCoords.y && y < emptyTileCoords.y + tileSize) {
+
+                const temp = field[colIndex][rowIndex];
+                field[colIndex][rowIndex] = 0;
+                field[emptyTile.col][emptyTile.row] = temp;
+                curTile = null;
+                moves++;
+                movesEl.textContent = moves;
+
+                if (state.tileSound){
+                    moveAudio.play();
+                }
+                gameOver = checkWin();
+                dragAndDropDone = true;
+            }
+        }
+    })
+}
+
+swipeDetect(canvas);
